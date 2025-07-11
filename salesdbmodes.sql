487d051b999328bf9bc69bd0c1c27aa7127c9e67
CoreMamba <215352410+CoreMamba@users.noreply.github.com>
2025-07-11 11:16:46 +0000

    Add salesdbmodes.sql

---
+++ salesdbmodes.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE IF NOT EXISTS salesdatabase;
+USE salesdatabase;
+
+CREATE TABLE weekly_sales_sheet (
+  `Weeks [W]` VARCHAR(50),
+  `Start date [SD]` DATE,
+  `End date [ED]` DATE,
+  `Days [Dy]` VARCHAR(20),
+  `No. of Days [NODy]` INT,
+  `Total days shop opened per week [TDSOPW]` INT,
+  `Total days shop closed per week [TDSCPW]` INT
+);
+
